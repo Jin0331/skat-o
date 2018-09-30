57bfc7c63bbdd3489a1f1539b8ad3c7ff6d1911c
Jin0331 <sempre813@hallym.ac.kr>
2018-10-01 02:11:19 +0900

    47_row까지 input 및 directory 수정

--- annovar_mulicore.R
+++ annovar_mulicore.R
@@ -7,41 +7,48 @@ install.packages("dplyr")
 library(glue);library(vcfR);library(data.table);library(foreach);library(doMC)
 library(dplyr)
 
-output <- "0928" ###### input output dir making--- yet
-core <- 7
-
-
+################################# row data #################################
 ### vcf-merge
 ## before vcftool using
-system("bgzip NABEC_EXOME_cohort_control.vcf");system("bgzip coriell_pd_exomes_case.vcf")
-system("tabix -p vcf NABEC_EXOME_cohort_control.vcf.gz");system("tabix -p vcf coriell_pd_exomes_case.vcf.gz")
-system("vcf-merge --remove-duplicates NABEC_EXOME_cohort_control.vcf.gz coriell_pd_exomes_case.vcf.gz | bgzip -c > merge_row.vcf.gz")
+system("bgzip /home/skat-0/row_data/NABEC_EXOME_cohort_control.vcf");system("bgzip /home/skat-0/row_data/coriell_pd_exomes_case.vcf")
+system("tabix -p vcf /home/skat-0/row_data/NABEC_EXOME_cohort_control.vcf.gz");system("tabix -p vcf /home/skat-0/row_data/coriell_pd_exomes_case.vcf.gz")
+system("vcf-merge --remove-duplicates /home/skat-0/row_data/NABEC_EXOME_cohort_control.vcf.gz /home/skat-0/row_data/coriell_pd_exomes_case.vcf.gz | bgzip -c > /home/skat-0/row_data/merge_row.vcf.gz")
 test <- read.vcfR(file = "merge_row.vcf.gz", convertNA = T, checkFile = F)
 test <- vcfR2tidy(test, info_only = T, single_frame = F, toss_INFO_column = T)
 test_fix <- test$fix;rm(test)
-fwrite(x = test_fix, file = "vcf_bind_0930_row_fix.txt", quote = F, sep = "\t", row.names = F, col.names = T, eol = "\n");rm(test_fix) ## fix out
+fwrite(x = test_fix, file = "/home/skat-0/row_data/vcf_bind_0930_row_fix.txt", quote = F, sep = "\t", row.names = F, col.names = T, eol = "\n");rm(test_fix) ## fix out
+################################# row data #################################
+
+
+
+core <- 7
+#### directory make
+setwd("~/skat-o/")
+date <- "0928"; date <- paste0(date, "_test") ######## date set, input !!!!!!!
+system(glue("mkdir {test_date}",test_date = date)) 
+setwd(paste0(getwd(), "/",date))
 
 #### vcf QC -- KGGSeq
-system("java -Xmx10g -jar /home/lee/kggseq10hg19/kggseq.jar --vcf-file /home/jinoo/skat-o/0914_skatT_duplicate.vcf.gz --ped-file /home/jinoo/skat-o/skato_0918.ped --out 0928_skatQC_KGGSeq --o-vcf --gty-qual 20 --gty-dp 8 --gty-sec-pl 20 --vcf-filter-in PASS --seq-qual 50 --seq-mg 20 --seq-fs 60 --hwe-control 1E-5 --nt 7")
-system("gzip -d 0928_skatQC_KGGSeq.flt.vcf.gz") ## bgzip and tabix
-system("bgzip 0928_skatQC_KGGSeq.flt.vcf")
-system("tabix -p vcf 0928_skatQC_KGGSeq.flt.vcf.gz")
-test <- read.vcfR(file = "0928_skatQC_KGGSeq.flt.vcf.gz", convertNA = T, checkFile = F)
+system("java -Xmx10g -jar /home/lee/kggseq10hg19/kggseq.jar --vcf-file /home/skat-0/row_data/merge_row.vcf.gz --ped-file /home/jinoo/skat-o/skato_0918.ped --out skatQC_KGGSeq --o-vcf --gty-qual 20 --gty-dp 8 --gty-sec-pl 20 --vcf-filter-in PASS --seq-qual 50 --seq-mg 20 --seq-fs 60 --hwe-control 1E-5 --nt 7")
+system("gzip -d skatQC_KGGSeq.flt.vcf.gz") ## bgzip and tabix
+system("bgzip skatQC_KGGSeq.flt.vcf")
+system("tabix -p vcf skatQC_KGGSeq.flt.vcf.gz")
+test <- read.vcfR(file = "skatQC_KGGSeq.flt.vcf.gz", convertNA = T, checkFile = F)
 test <- vcfR2tidy(test, info_only = T, single_frame = F, toss_INFO_column = T)
 test_fix <- test$fix;rm(test)
 fwrite(x = test_fix, file = "skatQC_KGGSeq_GVQC.txt", quote = F, sep = "\t", row.names = F, col.names = T, eol = "\n");rm(test_fix) ## fix out
 rm(test_fix)
 
 #### vcf QC -- vcftools
-system("vcftools --gzvcf 0928_skatQC.flt.vcf.gz --min-alleles 2 --max-alleles 2 --max-missing 0.8 --recode --out 0928_QC_80_allele_individual_flt")
-test <- read.vcfR(file = "", convertNA = T, checkFile = F)
+system("vcftools --gzvcf skatQC_KGGSeq.flt.vcf.gz --min-alleles 2 --max-alleles 2 --max-missing 0.8 --recode --out QC_80_allele_individual_vcftools_flt")
+test <- read.vcfR(file = "QC_80_allele_individual_vcftools_flt", convertNA = T, checkFile = F)
 test <- vcfR2tidy(test, info_only = T, single_frame = F, toss_INFO_column = T)
 test_fix <- test$fix;rm(test)
 fwrite(x = test_fix, file = "skatQC_allele_individual_flt_vcftools.txt", quote = F, sep = "\t", row.names = F, col.names = T, eol = "\n");rm(test_fix) ## fix out
 
 
 ## row merge & recode vcf load(7 div)
-temp <- fread(input = "0928_QC_80.recode.vcf", sep = "\t", quote = "\n", skip = 210, header = T, nThread = 14)
+temp <- fread(input = "QC_80_allele_individual_vcftools_flt.recode.vcf", sep = "\t", quote = "\n", skip = 210, header = T, nThread = 14)
 temp_tidy <- dplyr::tbl_df(temp);rm("temp")
 vcf_list <- list()
 vcf_list[1] <- list(temp_tidy[1:round(nrow(temp_tidy)/core),])
@@ -89,17 +96,17 @@ vcf_bind_complete <- rbind2(vcf_bind5, vcf_7);rm(vcf_7,vcf_bind5)
 # annotation fix & vcf out
 test <- vcfR2tidy(vcf_bind_complete, info_only = T, single_frame = F, toss_INFO_column = T)
 test_fix <- test$fix;rm(test)
-fwrite(x = test_fix, file = "vcf_bind_0928_80_fix.txt", quote = F, sep = "\t", row.names = F, col.names = T, eol = "\n") ## fix out
-write.vcf(x = vcf_bind_complete, file = "vcf_QC_0928_80.vcf.gz",APPEND = F);rm(vcf_bind_complete) ## vcf out
-system("gzip -d vcf_QC_0928_80.vcf.gz") ## bgzip and tabix
-system("bgzip vcf_QC_0928_80.vcf")
-system("tabix -p vcf vcf_QC_0928_80.vcf.gz")
+fwrite(x = test_fix, file = "annotation_fix.txt", quote = F, sep = "\t", row.names = F, col.names = T, eol = "\n") ## fix out
+write.vcf(x = vcf_bind_complete, file = "all_c_QC_80.vcf.gz",APPEND = F);rm(vcf_bind_complete) ## vcf out
+system("gzip -d all_c_QC_80.vcf.gz") ## bgzip and tabix
+system("bgzip all_c_QC_80.vcf")
+system("tabix -p vcf all_c_QC_80.vcf.gz")
 system("rm -rf ")
 
 
 
 ################### skat-o preprocessing #############################
-test_fix <- read.table(file = "vcf_bind_0917_fix.txt", sep = "\t", header = T, stringsAsFactors = F)
+test_fix <- read.table(file = "annotation_fix.txt", sep = "\t", header = T, stringsAsFactors = F)
 geneset <- read.csv("/home/jinoo/skat-o/parkinson_genset.txt", stringsAsFactors = F,header = F)
 geneset <- as.character(geneset[,1])
 
